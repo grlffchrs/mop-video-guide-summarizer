bf80c1ab39c352434a1834b07c505ac3684fc5e8
Christopher Gerloff <christophergerloff@Mac.fritz.box>
2025-09-22 08:49:58 +0200

    Remove LeiShi placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { shaOfFearGuide } from './modules/shaOfFear';
-import { leiShiGuide } from './modules/leiShi';
 import { jinrokhGuide } from './modules/jinrokh';
 import GuideModule from './components/GuideModule/GuideModule';
 import GuideNav from './components/GuideNav/GuideNav';
 import DesktopLayout from './components/Layout/DesktopLayout';
 
-const guides = [shaOfFearGuide, leiShiGuide, jinrokhGuide];
+const guides = [shaOfFearGuide, jinrokhGuide];
 
 function App() {
   const [currentGuide, setCurrentGuide] = useState(guides[0]);
